7fe6174e2c416e1dfc4171e9cfdcb0c07c93f8cd
hwangjello <jmhwang21@naver.com>
2023-02-09 01:09:57 +0900

    피보나치

---
+++ BOJ_Step/재귀/boj10870.java
@@ -0,0 +1,22 @@
+package BOJ_Step.재귀;
+import java.util.*;
+public class boj10870 {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		scan.close();
+		
+		int ans=fibonacci(n);
+		System.out.println(ans);
+	}
+	public static int fibonacci(int n) {
+		if(n==0) {
+			return 0;
+		}
+		else if(n==1) {
+			return 1;
+		}
+		return fibonacci(n-1)+fibonacci(n-2);
+	}
+}
