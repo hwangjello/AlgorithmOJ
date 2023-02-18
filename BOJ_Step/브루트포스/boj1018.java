ed703a64fead97bdaa8a112db54db1192302ec1b
hwangjello <jmhwang21@naver.com>
2023-02-18 16:48:38 +0900

    체스판 칠하기

--- BOJ_Step/브루트포스/boj1018.java
+++ BOJ_Step/브루트포스/boj1018.java
@@ -12,7 +12,7 @@ public class boj1018 {
 		
 		String [] arr=new String [n];
 		char [][] chess=new char [n][m];
-		
+		char [][] copy= chess;
 		for(int i=0;i<n;i++) {
 			arr[i]=scan.next();
 			for(int j=0;j<m;j++) {
@@ -21,11 +21,52 @@ public class boj1018 {
 		}
 		scan.close();
 		
-		int min4w,min4b;
+		int min4w=0;
+		int min4b=0;
 		
-		if(chess[0][0]=='W') {
-			
+		for(int i=0;i<n;i++) {
+			for(int j=1;j<m/2;j++) {
+				if(i%2==0) {
+					if(copy[i][2*j]!='B') {
+						min4w++;
+					}
+					if(copy[i][2*j-1]!='W') {
+						min4w++;
+					}
+				}
+				else {
+					if(copy[i][2*j]!='W') {
+						min4w++;
+					}
+					if(copy[i][2*j-1]!='B') {
+						min4w++;
+					}
+				}
+			}
 		}
+		for(int i=0;i<n;i++) {
+			for(int j=1;j<m/2;j++) {
+				if(i%2==0) {
+					if(copy[i][2*j]!='W') {
+						min4b++;
+					}
+					if(copy[i][2*j-1]!='B') {
+						min4b++;
+					}
+				}
+				else {
+					if(copy[i][2*j]!='B') {
+						min4b++;
+					}
+					if(copy[i][2*j-1]!='W') {
+						min4b++;
+					}
+				}
+			}
+		}
+		System.out.println(min4w+" "+min4b);
+		System.out.println(Math.min(min4w,min4b));
+		
 		
 	}
 
